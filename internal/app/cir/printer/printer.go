7d00a1a265612d37ad1cf9cd56f7de7e5efd0e9d
Michal Franc <michal.franc@form3.tech>
2021-04-04 20:22:54 +0100

    improved the printed layout

--- internal/app/cir/printer/printer.go
+++ internal/app/cir/printer/printer.go
@@ -1,23 +1,16 @@
 package printer
 
 import (
+	"fmt"
 	"github.com/liamg/tml"
 	"github.com/michal-franc/cir/internal/app/cir/analyser"
 )
 
-// non verbose just print yey it can
-// verbose
-// vpc-123456 (same vpc)
-// v -> sg - there is a outbound rule for source (ip)
-// v -> route - there is a route to destination in route table (id) -> (cidr range or id of gateway)
-// v -> route - there is a route from source in route table (id) -> (cidr range or id of gateway)
-// v -> sg - there is inboud rule for destination (ip)
-
-func printRedGreen(b bool, positive string, negative string) {
+func printRedGreen(message string, b bool) {
 	if b {
-		tml.Printf("<green>✓</green> -> %s\n", positive)
+		tml.Printf("<green>✓</green> -> %s\n", message)
 	} else {
-		tml.Printf("<red>×</red> -> %s\n", negative)
+		tml.Printf("<red>×</red> -> %s\n", message)
 	}
 }
 
@@ -36,14 +29,19 @@ func PrintAnalysis(analysis analyser.Analysis) {
 		tml.Println("(different vpcs)")
 	}
 
+	printRedGreen("security groups:", analysis.CanEnterDestination.IsPassing && analysis.CanEscapeSource.IsPassing)
 	printCheck(*analysis.CanEscapeSource)
+	printCheck(*analysis.CanEnterDestination)
+	fmt.Println()
+	printRedGreen("subnets:", analysis.SourceSubnetHasRoute.IsPassing && analysis.DestinationSubnetHasRoute.IsPassing)
 	printCheck(*analysis.SourceSubnetHasRoute)
-
+	if !analysis.AreInTheSameVpc { // display only dest subnet if diff vpc
+		printCheck(*analysis.DestinationSubnetHasRoute)
+	}
+	fmt.Println()
 	if !analysis.AreInTheSameVpc {
+		printRedGreen("vpc connection:", analysis.ConnectionBetweenVPCsIsActive.IsPassing && analysis.ConnectionBetweenVPCsIsValid.IsPassing)
 		printCheck(*analysis.ConnectionBetweenVPCsIsValid)
 		printCheck(*analysis.ConnectionBetweenVPCsIsActive)
 	}
-
-	printCheck(*analysis.DestinationSubnetHasRoute)
-	printCheck(*analysis.CanEnterDestination)
 }
