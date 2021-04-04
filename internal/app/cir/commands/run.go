e20ba8ab008e29367c621e96bf8ec7bac165b65c
Michal Franc <michal.franc@form3.tech>
2021-04-04 21:38:40 +0100

    adding params vallidation

--- internal/app/cir/commands/run.go
+++ internal/app/cir/commands/run.go
@@ -8,6 +8,8 @@ import (
 	"github.com/michal-franc/cir/internal/app/cir/printer"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"net"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/michal-franc/cir/internal/app/cir/scanner"
@@ -20,16 +22,52 @@ var debug bool
 
 func init() {
 	startCmd.Flags().StringVar(&sourceIp, "from", "", "Specifies which machine the communication is initiated from.")
+	startCmd.MarkFlagRequired("from")
 	startCmd.Flags().StringVar(&destinationIp, "to", "", "Specifies which machine the communication is destined to go to.")
+	startCmd.MarkFlagRequired("to")
 	startCmd.Flags().Int32Var(&port, "port", -1, "Specifies which port should be checked.")
+	startCmd.MarkFlagRequired("port")
 	startCmd.Flags().BoolVar(&debug, "debug", false, "Specifies if debug messages should be emitted.")
 	rootCmd.AddCommand(startCmd)
+}
+
+func validateIp(ip string, paramName string) bool {
+	sIp := net.ParseIP(ip)
+	if sIp == nil {
+		fmt.Printf("%s unable to parse ip\n", paramName)
+		return false
+	} else {
+		if sIp.To4() == nil {
+			fmt.Printf("%s param is ipv6 - not supported yet\n", paramName)
+			return false
+		}
+	}
+
+	return true
+}
+
+func validateArgs() bool {
+	isValid := true
+
+	if port <= 0 || port > 65535 {
+		fmt.Println("port value out of range 1-65535")
+		isValid = false
+	}
+
+	isValid = validateIp(sourceIp, "from") && isValid
+	isValid = validateIp(destinationIp, "to") && isValid
+
+	return isValid
 }
 
 var startCmd = &cobra.Command{
 	Use:   "run",
 	Short: "run analysis",
 	Run: func(cmd *cobra.Command, args []string) {
+		if !validateArgs() {
+			os.Exit(1)
+		}
+
 		log.SetLevel(log.WarnLevel)
 		fmt.Printf("checking if '%s' can reach '%s on port '%d'\n", sourceIp, destinationIp, port)
 
